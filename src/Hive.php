9e055d3e1d14e6ccf69320c6b8759364c2ae8f8c
lukemcgahan <l.mcgahan@swarmuk.com>
2019-06-15 23:27:39 +0100

    Composer stuff.

---
+++ src/Hive.php
@@ -0,0 +1,16 @@
+<?php
+namespace Swarm\Test;
+
+/**
+ * Test class.
+ */
+class Hive
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        // nothing yet
+    }
+}
